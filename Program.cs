8ac221e13a5a6a149b9e007a1739c07cf3c68bfa
trung6755 <trungggrr@gmail.com>
2024-03-05 21:03:53 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+        }
+    }
+}
